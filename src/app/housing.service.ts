6fa2787f7f1e03f6fd767e74523c573abbede19a
akist529 <akist0707@gmail.com>
2024-01-02 12:54:55 -0600

    Complete lesson 12 of first angular app tutorial

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -119,5 +119,9 @@ export class HousingService {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
   constructor() { }
 }
